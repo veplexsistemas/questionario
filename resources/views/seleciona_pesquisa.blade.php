a850d4b29551c963a97fc60e92d39ebd14e1a4b9
Matheus Lutz <matheus.lutz@veplex.com.br>
2019-03-29 16:38:17 -0300

    Melhorando listagem pesquisa

--- resources/views/seleciona_pesquisa.blade.php
+++ resources/views/seleciona_pesquisa.blade.php
@@ -16,14 +16,25 @@
     $html->addContent($panelStatus->make());
   }
   
-  //Grid
-  $grid = new VDataGrid();
-  $grid->setShowPagination(false);
-  $grid->setData($data);
-  $grid->setFields(["nm_pesquisa" => "Selecione uma pesquisa"]);
-  
-  $grid->addExtraField("", "<i class=\"fas fa-check\"></i> Responder", "/pesquisa/responder", ["cd_pesquisa"], "btn btn-primary btn-sm");
-  
-  $html->addObject($grid);
+  if (sizeof($data))
+  {
+    //Grid
+    $grid = new VDataGrid();
+    $grid->setShowPagination(false);
+    $grid->setData($data);
+    $grid->setFields(["nm_pesquisa" => "Selecione uma pesquisa"]);
+
+    $grid->addExtraField("", "<i class=\"fas fa-check\"></i> Responder", "/pesquisa/responder", ["cd_pesquisa"], "btn btn-primary btn-sm");
+
+    $html->addObject($grid);
+  }
+  else
+  {
+    $panel = new VPanel();
+    $panel->setClass("panel panel-info");
+    $panel->addHeading("<i class=\"fas fa-info-circle\"></i> Informação");
+    $panel->addBody("Você não possui pesquisas para responder no momento.");
+    $html->addObject($panel);
+  }
   
   echo $html->make();
